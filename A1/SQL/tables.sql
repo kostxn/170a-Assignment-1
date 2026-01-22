b0825f15e132d1b54f8e5ccf58156660a1a73434
Travis Nguyen <travisgnguyen21@gmail.com>
2026-01-21 17:05:25 -0800

    Alternate SQL table creation
    replace the path with your own file path

--- A1/SQL/tables.sql
+++ A1/SQL/tables.sql
@@ -54,6 +54,33 @@ CREATE TABLE author_info (
 	) STORED
 );
 
+/*
+copy public."generated-author-info"(name, dept, area, paper_count, adjustedcount, year)
+FROM 'C:\Users\travi\OneDrive\Desktop\Stats170A\170A Homework 1 Data\generated-author-info.csv'
+WITH (
+    FORMAT csv,
+    HEADER true,
+    DELIMITER ',',
+    QUOTE '"'
+);
+
+ALTER TABLE "generated-author-info"
+ADD COLUMN first_name TEXT,
+ADD COLUMN middle_name TEXT,
+ADD COLUMN last_name TEXT;
+
+UPDATE "generated-author-info"
+SET
+    first_name = split_part(name, ' ', 1),
+    last_name  = split_part(name, ' ', array_length(string_to_array(name, ' '), 1)),
+    middle_name =
+        CASE
+            WHEN array_length(string_to_array(name, ' '), 1) > 2
+            THEN split_part(name, ' ', 2)
+            ELSE NULL
+        END;
+*/
+
 CREATE TABLE acm_fellows (
 	name TEXT,
 	year INT
@@ -109,6 +136,46 @@ CREATE TABLE data (
     sat_range TEXT,
     act_range TEXT
 );
+
+/*
+CREATE TABLE data_raw (
+    institution_displayname TEXT,
+    institution_schooltype TEXT,
+    institution_aliasnames TEXT,
+    institution_state TEXT,
+    institution_city TEXT,
+    institution_zip TEXT,
+    institution_region TEXT,
+    institution_ispublic TEXT,
+    institution_institutionalcontrol TEXT,
+    institution_primaryphotocardthumb TEXT,
+    ranking_displayrank TEXT,
+    ranking_sortrank TEXT,
+    ranking_istied TEXT,
+    searchdata_actavg_rawvalue TEXT,
+    searchdata_percentreceivingaid_rawvalue TEXT,
+    searchdata_acceptancerate_rawvalue TEXT,
+    searchdata_tuition_rawvalue TEXT,
+    searchdata_hsgpaavg_rawvalue TEXT,
+    searchdata_engineeringrepscore_rawvalue TEXT,
+    searchdata_parentrank_rawvalue TEXT,
+    searchdata_enrollment_rawvalue TEXT,
+    searchdata_businessrepscore_rawvalue TEXT,
+    searchdata_satavg_rawvalue TEXT,
+    searchdata_costafteraid_rawvalue TEXT,
+    searchdata_testavgs_displayvalue_0_value TEXT,
+    searchdata_testavgs_displayvalue_1_value TEXT
+);
+
+COPY data_raw
+FROM 'C:\Users\travi\OneDrive\Desktop\Stats170A\170A Homework 1 Data\data.csv'
+WITH (
+    FORMAT csv,
+    HEADER true,
+    DELIMITER ',',
+    QUOTE '"'
+);
+*/
 
 CREATE TABLE field_conference (
 	major TEXT,
